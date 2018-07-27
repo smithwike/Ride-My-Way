73d4074b19a410681b9ed000ad6a8e7a2683e1d4
rajeman <rojo.mosco@gmail.com>
2018-07-27 21:31:38 +0100

    Remove dist/ directory from git

--- dist/models/rideOffers.js
+++ dist/models/rideOffers.js
@@ -4,12 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var rideOffers = [{
-  id: 1,
+  id: 'qruhn9b5h4jk4f8uiw',
   destination: 'Lokoja',
   takeOffTime: '12:30pm',
   totalSeats: 12
 }, {
-  id: 2,
+  id: 'qruhn9b5h4jk4f8tie',
   destination: 'Lagos',
   takeOffTime: '1:30am',
   totalSeats: 15
