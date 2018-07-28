e4e4e1d1a81821e7953217d647fce1a8cd27e75a
rajeman <rojo.mosco@gmail.com>
2018-07-28 10:34:00 +0100

    Remove dummy tests

--- src/algorithm.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const square = a => a * a;
-
-console.log(add(1, 3));
-console.log(subtract(1, 3));
-console.log(square(5));
-
-module.exports = { add, subtract, square };
