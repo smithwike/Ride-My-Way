11670f7b4889b2e29a4501dee0c5fc5153098911
rajeman <rojo.mosco@gmail.com>
2018-07-29 11:28:59 +0100

    Create auth route with db

--- src/models/db-query.js
+++ src/models/db-query.js
@@ -1,18 +1,18 @@
 import { Client } from 'pg';
 
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/books';
-const client = new Client(connectionString);
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ride-my-way';
 const usersTable = 'users';
 // const orderTable = 'orders';
 
 
 const getAll = () => new Promise((resolve, reject) => {
+  const client = new Client(connectionString);
   client.connect()
     .then(() => {
       const sql = `SELECT * FROM ${usersTable}`;
       client.query(sql)
         .then((result) => {
-          console.log(result.rows);
+          // console.log(result.rows);
           resolve(result.rows);
           client.end();
         })
@@ -20,4 +20,40 @@ const getAll = () => new Promise((resolve, reject) => {
     }).catch(e => reject(e));
 });
 
-export default getAll;
+const createUser = item => new Promise((resolve, reject) => {
+  const client = new Client(connectionString);
+  client.connect()
+    .then(() => {
+      const sql = `INSERT INTO ${usersTable} (user_email, user_password, user_name) VALUES ($1, $2, $3)`;
+      const params = [item.email, item.password, item.name];
+      client.query(sql, params)
+        .then((result) => {
+          // console.log(result.rows);
+          resolve(result.rowCount);
+          client.end();
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    }).catch((e) => {
+      reject(e);
+    });
+});
+
+const getUser = email => new Promise((resolve, reject) => {
+  const client = new Client(connectionString);
+  client.connect()
+    .then(() => {
+      const sql = `SELECT * FROM ${usersTable} WHERE user_email = $1;`;
+      const params = [email];
+      client.query(sql, params)
+        .then((result) => {
+          resolve(result.rows);
+          client.end();
+        })
+        .catch(e => reject(e));
+    }).catch(e => reject(e));
+});
+
+
+export { getAll, createUser, getUser };
