7b96dd2af1a41175ffa431a5714bec53bdc4304a
rajeman <rojo.mosco@gmail.com>
2018-07-29 21:22:23 +0100

    Remove SSL from client object

--- src/models/db-query.js
+++ src/models/db-query.js
@@ -6,7 +6,7 @@ const usersTable = 'users';
 
 
 const getAll = () => new Promise((resolve, reject) => {
-  const client = new Client({ connectionString, ssl: true });
+  const client = new Client(connectionString);
   client.connect()
     .then(() => {
       const sql = `SELECT * FROM ${usersTable}`;
@@ -21,7 +21,7 @@ const getAll = () => new Promise((resolve, reject) => {
 });
 
 const createUser = item => new Promise((resolve, reject) => {
-  const client = new Client({ connectionString, ssl: true });
+  const client = new Client(connectionString);
   client.connect()
     .then(() => {
       const sql = `INSERT INTO ${usersTable} (user_email, user_password, user_name) VALUES ($1, $2, $3)`;
@@ -41,7 +41,7 @@ const createUser = item => new Promise((resolve, reject) => {
 });
 
 const getUser = email => new Promise((resolve, reject) => {
-  const client = new Client({ connectionString, ssl: true });
+  const client = new Client(connectionString);
   client.connect()
     .then(() => {
       const sql = `SELECT * FROM ${usersTable} WHERE user_email = $1;`;
@@ -60,3 +60,4 @@ export { getAll, createUser, getUser };
 
 
 // CREATE TABLE users(user_id serial PRIMARY KEY, user_name text NOT NULL, user_email text UNIQUE NOT NULL, user_password text NOT NULL);
+// const client = new Client({ connectionString, ssl: true });
