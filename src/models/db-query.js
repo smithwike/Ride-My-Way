5f64295e9481978f8a2861933192dd2afffe7b1a
rajeman <rojo.mosco@gmail.com>
2018-07-29 20:09:00 +0100

    Add table create query

--- src/models/db-query.js
+++ src/models/db-query.js
@@ -57,3 +57,6 @@ const getUser = email => new Promise((resolve, reject) => {
 
 
 export { getAll, createUser, getUser };
+
+
+// CREATE TABLE users(user_id serial PRIMARY KEY, user_name text NOT NULL, user_email text UNIQUE NOT NULL, user_password text NOT NULL);
